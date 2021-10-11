51ad7ed63cc16a2c415c494a48514db0bf0f7103
nickdpz <ndpastranzamora@gmail.com>
2021-10-10 23:18:01 -0500

    Updates

---
+++ precache-manifest.d847c4ff07a136088a384e7c2c4a9ba1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99671f1b1c9c061af812",
+    "url": "/css/app.da71181c.css"
+  },
+  {
+    "revision": "9756e702d273f32f0879",
+    "url": "/css/chunk-vendors.ec4a1505.css"
+  },
+  {
+    "revision": "2525f9235ebfca2690360a373c33581f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99671f1b1c9c061af812",
+    "url": "/js/app.83e67fea.js"
+  },
+  {
+    "revision": "9756e702d273f32f0879",
+    "url": "/js/chunk-vendors.f9e39241.js"
+  },
+  {
+    "revision": "4072eee82d0e537ad81c10f647353616",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
