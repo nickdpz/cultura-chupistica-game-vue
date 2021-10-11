c70d7cdd60a5c5a5e67a70748e01651b327ded53
nickdpz <ndpastranzamora@gmail.com>
2021-10-10 23:35:50 -0500

    Updates

--- precache-manifest.bea0ab3121bb5ab8dcdcc36f052e4afe.js
+++ precache-manifest.bea0ab3121bb5ab8dcdcc36f052e4afe.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32b67f2ed6eb2e92cdc8",
+    "revision": "4b3b55c0cc774ccbfc2f",
     "url": "/cultura-chupistica/css/app.da71181c.css"
   },
   {
@@ -8,19 +8,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cultura-chupistica/css/chunk-vendors.ec4a1505.css"
   },
   {
-    "revision": "45e152811593bac577153fc2d1905a64",
+    "revision": "54b1aa4635dc18324d9b971291413381",
     "url": "/cultura-chupistica/index.html"
   },
   {
-    "revision": "32b67f2ed6eb2e92cdc8",
-    "url": "/cultura-chupistica/js/app.8e54c043.js"
+    "revision": "4b3b55c0cc774ccbfc2f",
+    "url": "/cultura-chupistica/js/app.ff777559.js"
   },
   {
     "revision": "f8a0fbd50b7d3e4dca11",
     "url": "/cultura-chupistica/js/chunk-vendors.ddd38931.js"
   },
   {
-    "revision": "4072eee82d0e537ad81c10f647353616",
+    "revision": "f440385001b03778862194c3219e9952",
     "url": "/cultura-chupistica/manifest.json"
   },
   {
--- registerServiceWorker.js
+++ registerServiceWorker.js
@@ -1,4 +1,4 @@
-importScripts("/cultura-chupistica/precache-manifest.2b0486d4803a2e1fba566cbe18aa89d2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/cultura-chupistica/precache-manifest.bea0ab3121bb5ab8dcdcc36f052e4afe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-console */
 
